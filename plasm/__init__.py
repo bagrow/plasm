4818cba0248489507175923d69db0520811f900c
Jim Bagrow <bagrowjp@gmail.com>
2021-05-10 13:29:49 -0400

    unify version __version__

--- plasm/__init__.py
+++ plasm/__init__.py
@@ -1,1 +1,2 @@
+__version__ = "1.0.dev1"
 from .plasm import *
--- setup.py
+++ setup.py
@@ -3,16 +3,34 @@
 
 # setup.py
 # Jim Bagrow
-# Last Modified: 2021-05-07
+# Last Modified: 2021-05-10
 
+import codecs
+import os.path
 import setuptools
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="PLASM",
-    version="1.0.dev0",
+    version=get_version("plasm/__init__.py"),
     author="Jim Bagrow",
     author_email="bagrowjp@gmail.com",
     description="PLot Analysis Spreads for Meetings",
